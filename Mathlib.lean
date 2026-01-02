2e6308f087736fd4b2ece82bf45267c3693473a6
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2026-01-02 19:42:27 +1100

    Bump

--- Mathlib.lean
+++ Mathlib.lean
@@ -5685,6 +5685,7 @@ public import Mathlib.Order.Nucleus
 public import Mathlib.Order.OmegaCompletePartialOrder
 public import Mathlib.Order.OrdContinuous
 public import Mathlib.Order.OrderIsoNat
+public import Mathlib.Order.OrderType
 public import Mathlib.Order.PFilter
 public import Mathlib.Order.Part
 public import Mathlib.Order.PartialSups
