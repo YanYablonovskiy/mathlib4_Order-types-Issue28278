e9e9e6519ea031891918995a47f2cdfc40b30a31
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2026-01-06 01:11:04 +1100

    Change mathlib imports from `OrderType`
    `OrderType.lean` no longer exists

--- Mathlib.lean
+++ Mathlib.lean
@@ -5603,7 +5603,6 @@ public import Mathlib.Order.Nucleus
 public import Mathlib.Order.OmegaCompletePartialOrder
 public import Mathlib.Order.OrdContinuous
 public import Mathlib.Order.OrderIsoNat
-public import Mathlib.Order.OrderType
 public import Mathlib.Order.PFilter
 public import Mathlib.Order.Part
 public import Mathlib.Order.PartialSups
@@ -5653,6 +5652,8 @@ public import Mathlib.Order.SymmDiff
 public import Mathlib.Order.Synonym
 public import Mathlib.Order.TeichmullerTukey
 public import Mathlib.Order.TransfiniteIteration
+public import Mathlib.Order.Types.Defs
+public import Mathlib.Order.Types.Basic
 public import Mathlib.Order.TypeTags
 public import Mathlib.Order.ULift
 public import Mathlib.Order.UpperLower.Basic
